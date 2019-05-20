3b1e31bc0ea3ecac35bbece1c6698c4c227d4aea
Gumbx <gustavo.bonassa1@gmail.com>
2019-05-20 16:34:10 -0300

    First commit

---
+++ docs/commands.md
@@ -0,0 +1,3 @@
+# Command Reference for gumb
+
+TODO: Add your command reference here
---
+++ src/extensions/create-component.js
@@ -0,0 +1,27 @@
+module.exports = (toolbox) => {
+    const { filesystem, template, print: {success,error} } = toolbox;
+
+    async function isReactNative(){
+        const package = await filesystem.read('package.json', 'json');
+        return !!package.dependencies['react-native'];
+    }
+    async function createComponent(folder, name){
+        if(!name) {
+            error('Name must be specified');
+            return
+        }
+        await template.generate({
+            template: 'component.js.ejs',
+            target: `${folder}/${name}/index.js`,
+            props: { name },
+        })
+        const stylesTemplate = (await isReactNative()) ? 'styles-rn.js.ejs' : 'styles-react.js.ejs';
+        await template.generate({
+            template: stylesTemplate,
+            target: `${folder}/${name}/styles.js`,
+        })
+
+        success(`Generated ${folder}/${name}.`)
+    }
+    toolbox.createComponent = createComponent;
+};
